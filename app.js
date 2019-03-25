027b2fc3cc5e96cb1f0f87105846b1f8892f732a
Dylan Macalast <dylanmacalast@dylans-mbp.home>
2019-03-25 20:33:34 +0000

    this is creating genres

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
+const jazz = document.getElementById('jazz');
+const rock = document.getElementById('rock');
+const folk = document.getElementById('folk');
+const all = document.getElementById('all');
+
+jazz.addEventListener('click', () => createList('jazz'));
+rock.addEventListener('click', () => createList('rock'));
+folk.addEventListener('click', () => createList('folk'));
+all.addEventListener('click', () => createList(''));
+
+
+
+activeLink = () => {
+
+}
+
 getData = async () => {
     const result =  await fetch("data/shows.JSON");
     const resultJson = await result.json();
     return resultJson;
 }
 
-createList = async () => {
+
+clearData = () => {
+    document.querySelector('.list').innerHTML = '';
+  
+}
+
+
+
+
+// genre as a parameter.
+createList = async (genreToFilterBy) => {
+    clearData();
     const data = await getData();
-    const theEvents = data.map(event => {
+    const filteredData = data.filter(event => event.genre === genreToFilterBy);
+    console.log(filteredData);
+    
+    const dataToBeMapped = filteredData.length > 0 ? filteredData : data;
+
+    const theEvents = dataToBeMapped.map(event => {
         return (
             `<li> ${event.date.dayOfWeek} ${event.location}  </li> 
             <li> ${event.date.dayOfMonth} ${event.date.month}</li> <br>
             `
         );
-
     });
+    
+
+
     attachedToBody(theEvents);
 };
 
@@ -23,18 +57,14 @@ attachedToBody = (data) => {
 };
 
 
-createList();
+createList("");
 
 
 
-getGenre = async () => {
-    const data = await getData();
-    const getGenre = data.map(e => {
-        return e.genre;
-    });
 
-    console.log(getGenre);
-};
 
-getGenre();
 
+// loop through all the data
+    // grab the unique genres
+        // loop over the unique genres
+            // generating button that has an id / data param with it's genre type
